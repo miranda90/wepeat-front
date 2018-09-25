40abcebe957e7d49df9305213ae396c199eae38c
MartinM <martin.miranda@invbit.es>
2018-09-26 01:34:23 +0200

    Maquetación página búsqueda

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -20,7 +20,7 @@
       <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
     </ul>
     <h3>Ecosystem</h3>
-  
+
   </div>
 </template>
 
--- src/views/Search.vue
+++ src/views/Search.vue
@@ -29,93 +29,117 @@
       </div>
     </div>
     <div class="search-results">
-      <div class="card">
-        <div class="card-img">
-          <img src="../assets/card.png" class="img-responsive" alt="">
+      <div class="search-results_info">
+        <h3 class="search-results_title title-md">Se han encontrado 10 resultados</h3>
+      </div>
+      <div class="search-results_inner">
+        <div class="card">
+          <div class="card-img">
+            <img src="../assets/meal.jpg" class="img-responsive" alt="">
+          </div>
+          <div class="card-content">
+            <a href="" class="badge badge-card badge--plan">Planificador</a>
+            <h3 class="card-title"><a href="#">Carne guisada con patatas</a></h3>
+            <a href="" class="card-category">Category card</a>
+            <p class="card-excerpt">Lorem ipsum dolor sit amet, consect adipisci elit. condimentum suscipic.</p>
+          </div>
         </div>
-        <div class="card-content">
-          <a href="" class="badge badge-card badge--plan">Planificador</a>
-          <h3 class="card-title">Card title</h3>
-          <a href="" class="card-category">Category card</a>
-          <p class="card-excerpt">Lorem ipsum dolor sit amet, consect adipisci elit. condimentum suscipic.</p>
+        <div class="card">
+          <div class="card-img">
+            <img src="../assets/meal.jpg" class="img-responsive" alt="">
+          </div>
+          <div class="card-content">
+            <a href="" class="badge badge-card badge--plan">Planificador</a>
+            <h3 class="card-title"><a href="#">Carne guisada con patatas</a></h3>
+            <a href="" class="card-category">Category card</a>
+            <p class="card-excerpt">Lorem ipsum dolor sit amet, consect adipisci elit. condimentum suscipic.</p>
+          </div>
+        </div>
+        <div class="card">
+          <div class="card-img">
+            <span class="badge badge-card_info badge--plan">30 días</span>
+            <img src="../assets/meal.jpg" class="img-responsive" alt="">
+          </div>
+          <div class="card-content">
+            <a href="" class="badge badge-card badge--plan">Planificador</a>
+            <h3 class="card-title"><a href="#">Carne guisada con patatas</a></h3>
+            <a href="" class="card-category">Category card</a>
+            <p class="card-excerpt">Lorem ipsum dolor sit amet, consect adipisci elit. condimentum suscipic.</p>
+          </div>
+        </div>
+        <div class="card card--premium">
+          <div class="card-img">
+            <img src="../assets/meal.jpg" class="img-responsive" alt="">
+          </div>
+          <div class="card-content">
+            <a href="" class="badge badge-card badge--premium">Planificador</a>
+            <h3 class="card-title"><a href="#">Carne guisada con patatas</a></h3>
+            <a href="" class="card-category">Category card</a>
+            <p class="card-excerpt">Lorem ipsum dolor sit amet, consect adipisci elit. condimentum suscipic.</p>
+          </div>
+        </div>
+        <div class="card">
+          <div class="card-img">
+            <img src="../assets/meal.jpg" class="img-responsive" alt="">
+          </div>
+          <div class="card-content">
+            <a href="" class="badge badge-card badge--plan">Planificador</a>
+            <h3 class="card-title"><a href="#">Carne guisada con patatas</a></h3>
+            <a href="" class="card-category">Category card</a>
+            <p class="card-excerpt">Lorem ipsum dolor sit amet, consect adipisci elit. condimentum suscipic.</p>
+          </div>
+        </div>
+        <div class="card">
+          <div class="card-img">
+            <img src="../assets/meal.jpg" class="img-responsive" alt="">
+          </div>
+          <div class="card-content">
+            <a href="" class="badge badge-card badge--plan">Planificador</a>
+            <h3 class="card-title"><a href="#">Carne guisada con patatas</a></h3>
+            <a href="" class="card-category">Category card</a>
+            <p class="card-excerpt">Lorem ipsum dolor sit amet, consect adipisci elit. condimentum suscipic.</p>
+          </div>
+        </div>
+        <div class="card">
+          <div class="card-img">
+            <img src="../assets/meal.jpg" class="img-responsive" alt="">
+          </div>
+          <div class="card-content">
+            <a href="" class="badge badge-card badge--plan">Planificador</a>
+            <h3 class="card-title"><a href="#">Carne guisada con patatas</a></h3>
+            <a href="" class="card-category">Category card</a>
+            <p class="card-excerpt">Lorem ipsum dolor sit amet, consect adipisci elit. condimentum suscipic.</p>
+          </div>
+        </div>
+        <div class="card">
+          <div class="card-img">
+            <img src="../assets/meal.jpg" class="img-responsive" alt="">
+          </div>
+          <div class="card-content">
+            <a href="" class="badge badge-card badge--recipe">Receta</a>
+            <h3 class="card-title"><a href="#">Carne guisada con patatas</a></h3>
+            <a href="" class="card-category">Category card</a>
+            <p class="card-excerpt">Lorem ipsum dolor sit amet, consect adipisci elit. condimentum suscipic.</p>
+          </div>
         </div>
       </div>
-      <div class="card">
-        <div class="card-img">
-          <img src="../assets/card.png" class="img-responsive" alt="">
+      <div class="banners-block banners-6">
+        <div class="banner">
+          <img src="../assets/banner.jpg" alt="" class="img-responsive">
+          <div class="banner-content">
+            <div class="banner-title">Banner title</div>
+            <img src="../assets/line.svg" alt="" class="banner-line">
+            <a href="" class="banner-link">Descúbrelo</a>
+          </div>
         </div>
-        <div class="card-content">
-          <a href="" class="badge badge-card badge--plan">Planificador</a>
-          <h3 class="card-title">Card title</h3>
-          <a href="" class="card-category">Category card</a>
-          <p class="card-excerpt">Lorem ipsum dolor sit amet, consect adipisci elit. condimentum suscipic.</p>
-        </div>
-      </div>
-      <div class="card">
-        <div class="card-img">
-          <span class="badge badge-card_info badge--plan">30 días</span>
-          <img src="../assets/card.png" class="img-responsive" alt="">
-        </div>
-        <div class="card-content">
-          <a href="" class="badge badge-card badge--plan">Planificador</a>
-          <h3 class="card-title">Card title</h3>
-          <a href="" class="card-category">Category card</a>
-          <p class="card-excerpt">Lorem ipsum dolor sit amet, consect adipisci elit. condimentum suscipic.</p>
-        </div>
-      </div>
-      <div class="card card--premium">
-        <div class="card-img">
-          <img src="../assets/card.png" class="img-responsive" alt="">
-        </div>
-        <div class="card-content">
-          <a href="" class="badge badge-card badge--premium">Planificador</a>
-          <h3 class="card-title">Card title</h3>
-          <a href="" class="card-category">Category card</a>
-          <p class="card-excerpt">Lorem ipsum dolor sit amet, consect adipisci elit. condimentum suscipic.</p>
-        </div>
-      </div>
-      <div class="card">
-        <div class="card-img">
-          <img src="../assets/card.png" class="img-responsive" alt="">
-        </div>
-        <div class="card-content">
-          <a href="" class="badge badge-card badge--plan">Planificador</a>
-          <h3 class="card-title">Card title</h3>
-          <a href="" class="card-category">Category card</a>
-          <p class="card-excerpt">Lorem ipsum dolor sit amet, consect adipisci elit. condimentum suscipic.</p>
-        </div>
-      </div>
-      <div class="card">
-        <div class="card-img">
-          <img src="../assets/card.png" class="img-responsive" alt="">
-        </div>
-        <div class="card-content">
-          <a href="" class="badge badge-card badge--plan">Planificador</a>
-          <h3 class="card-title">Card title</h3>
-          <a href="" class="card-category">Category card</a>
-          <p class="card-excerpt">Lorem ipsum dolor sit amet, consect adipisci elit. condimentum suscipic.</p>
-        </div>
-      </div>
-      <div class="card">
-        <div class="card-img">
-          <img src="../assets/card.png" class="img-responsive" alt="">
-        </div>
-        <div class="card-content">
-          <a href="" class="badge badge-card badge--plan">Planificador</a>
-          <h3 class="card-title">Card title</h3>
-          <a href="" class="card-category">Category card</a>
-          <p class="card-excerpt">Lorem ipsum dolor sit amet, consect adipisci elit. condimentum suscipic.</p>
-        </div>
-      </div>
-      <div class="card">
-        <div class="card-img">
-          <img src="../assets/card.png" class="img-responsive" alt="">
-        </div>
-        <div class="card-content">
-          <a href="" class="badge badge-card badge--recipe">Receta</a>
-          <h3 class="card-title">Card title</h3>
-          <a href="" class="card-category">Category card</a>
-          <p class="card-excerpt">Lorem ipsum dolor sit amet, consect adipisci elit. condimentum suscipic.</p>
+        <div class="banner">
+          <img src="../assets/banner2.jpg" alt="" class="img-responsive">
+          <div class="banner-content">
+            <span class="badge badge-card badge--recipe">New collection</span>
+            <div class="banner-title">Banner title</div>
+            <img src="../assets/line.svg" alt="" class="banner-line">
+            <a href="" class="banner-link">Descúbrelo</a>
+          </div>
         </div>
       </div>
     </div>
