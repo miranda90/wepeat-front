30e023805ded01dc3723132de163656fbb2a6bda
MartinM <martin.miranda@invbit.es>
2018-09-27 22:31:10 +0200

    Maquetación buscador portada

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,36 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="search-cover">
+    <div class="search-cover_content">
+      <h1 class="search-cover_title">
+        ¿Estás buscando un planificador?
+      </h1>
+      <h5 class="search-cover_subtitle">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus ex nec
+        libero iaculis vulputate</h5>
+    </div>
+    <div class="search-cover_searchbox">
+      <form action="" method="get">
+        <div class="search-cover_searchbox_block">
+          <input type="text" class="search-cover_searchbox_input" placeholder="Buscar por planificador, receta, tags...">
+          <button class="search-cover_searchbox_btn" type="submit">
+            <svg width="25" height="25" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+              stroke-linejoin="round">
+              <use xlink:href="feather-sprite.svg#search" />
+            </svg>
+          </button>
+        </div>
+      </form>
+    </div>
+    <img src="../assets/meal2-png.png" alt="" class="search-cover_img search-cover_img--left">
+    <img src="../assets/meal-png.png" alt="" class="search-cover_img search-cover_img--right">
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
-
-export default {
-  name: 'home',
-  components: {
-    HelloWorld,
-  },
-};
-</script>
+<style>
+.nav--main {
+  background: none;
+  position: fixed;
+  top: 0;
+  width: 100%;
+  z-index: 9;
+}
+</style>
